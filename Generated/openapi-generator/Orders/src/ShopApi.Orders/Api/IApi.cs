d9dd21a820156b5e156ce498df9b6c847d4b4257
dimka <dima.kazankov@gmail.com>
2025-09-27 22:08:27 +0200

    Remove generated files for Inventory, Orders, and Users APIs, including .gitignore, .openapi-generator-ignore, configuration files, and source code files. This cleanup eliminates unnecessary artifacts from the project structure.

--- Generated/openapi-generator/Orders/src/ShopApi.Orders/Api/IApi.cs
+++
@@ -1,9 +0,0 @@
-using System.Net.Http;
-
-namespace ShopApi.Orders.Api
-{
-    public interface IApi
-    {
-        HttpClient HttpClient { get; }
-    }
-}
--- Generated/openapi-generator/Users/src/ShopApi.Users/Api/IApi.cs
+++
@@ -1,9 +0,0 @@
-using System.Net.Http;
-
-namespace ShopApi.Users.Api
-{
-    public interface IApi
-    {
-        HttpClient HttpClient { get; }
-    }
-}
